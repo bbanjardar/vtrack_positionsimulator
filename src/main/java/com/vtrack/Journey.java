f555dc260ff8cac54f241df899eb37c647661956
Noorita <kaur.navneeth@gmail.com>
2018-08-16 19:26:08 -0400

    renamed packages

--- src/main/java/com/vtrack/Journey.java
+++ src/main/java/com/vtrack/Journey.java
@@ -1,4 +1,4 @@
-package com.virtualpairprogrammers;
+package com.vtrack;
 
 import java.util.Collections;
 import java.util.HashMap;
--- src/main/java/com/vtrack/JourneySimulator.java
+++ src/main/java/com/vtrack/JourneySimulator.java
@@ -1,4 +1,4 @@
-package com.virtualpairprogrammers;
+package com.vtrack;
 
 import java.io.File;
 import java.io.IOException;
